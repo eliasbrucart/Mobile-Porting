7958e8cf4bc56f566a8715afa360a43d9f5d4ed8
Elias Brucart <eliasbrucart@hardgames.com.ar>
2020-09-14 11:24:38 -0300

    Menu layout ready

--- Assets/SCRIPTS/MainMenu.cs
+++ Assets/SCRIPTS/MainMenu.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject[] buttons;
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+            return;
+        EventSystem.current.SetSelectedGameObject(buttons[0]);
+    }
+
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
