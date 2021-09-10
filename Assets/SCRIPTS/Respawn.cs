97be1e7702be52d721b1b63269eb80c453522e30
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-09-10 01:35:04 -0300

    Added menu scene

--- Assets/SCRIPTS/Respawn.cs
+++ Assets/SCRIPTS/Respawn.cs
@@ -65,7 +65,7 @@ public class Respawn : MonoBehaviour
 	{
 		GetComponent<Rigidbody>().velocity = Vector3.zero;
 		
-		gameObject.SendMessage("SetGiro", 0f);
+		gameObject.SendMessage("SetGiro", 1f);
 		
 		if(CPAct.Habilitado())
 		{
