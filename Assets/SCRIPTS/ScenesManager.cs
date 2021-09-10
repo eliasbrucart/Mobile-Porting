71e533a9fe14727a23e0e7bc4b0714a2c2f02239
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-09-10 12:32:15 -0300

    First version of the menu

---
+++ Assets/SCRIPTS/ScenesManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
