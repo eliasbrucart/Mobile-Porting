fd53a0e469ffab12611e453992f3d6a30c1c2b36
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-09-15 10:51:06 -0300

    Added input by tutorial

--- Assets/SCRIPTS/Game/Tutorial/Tutorial.cs
+++ Assets/SCRIPTS/Game/Tutorial/Tutorial.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
+    public enum SelectedInput
+    {
+        WASD,
+        Arrows
+    }
+    public SelectedInput selectedInput;
     public enum StatesTutorial { Calibrando, Tutorial, Finalizado }
     public StatesTutorial EstAct = StatesTutorial.Calibrando;
     void Start()
@@ -11,6 +18,37 @@ public class Tutorial : MonoBehaviour
 
     void Update()
     {
-        
+        TutorialInput();
+    }
+
+    private void TutorialInput()
+    {
+        if(selectedInput == SelectedInput.Arrows)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+                EstAct = StatesTutorial.Tutorial;
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                EstAct = StatesTutorial.Finalizado;
+                CheckTutorialFinish();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W))
+                EstAct = StatesTutorial.Tutorial;
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                EstAct = StatesTutorial.Finalizado;
+                CheckTutorialFinish();
+            }
+        }
+    }
+
+    private void CheckTutorialFinish()
+    {
+        if (EstAct == StatesTutorial.Finalizado)
+            SceneManager.LoadScene("conduccion9");
+        //chequear que tutorial termino para darle velocidad al camion correspondiente
     }
 }
